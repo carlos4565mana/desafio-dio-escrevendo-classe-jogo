03cb55c86c67fbf9e64caa4f2bd302c839309206
carlos4565mana <carloscal61@gmail.com>
2024-02-04 18:33:04 -0200

    first commit

---
+++ desafio.js
@@ -0,0 +1,27 @@
+class Hero {
+  // type: 0 = mago, 1 = guerreiro, 2 = monge, 3 = ninja;
+  constructor(name, age, type) {
+      this.name = name;
+      this.age = age;
+
+      this.types = ["mago", "guerreiro", "monge", "ninja"];
+      this.attacks = ["magia", "espada", "artes marciais", "shuriken"]
+      this.type = type % this.types.length;
+  }
+  getType() {
+      return this.types[this.type];
+  }
+  getAttack() {
+      return this.attacks[this.type];
+  }
+  attack() {
+      console.log(`O ${this.getType()} atacou usando ${this.getAttack()}`);
+  }
+  info() {
+      console.log(`Nome: ${this.name}, idade: ${this.age}, Tipo: ${this.getType()}`);
+  }
+}
+
+for(let i = 0; i < 4; i++){
+  let carlos = new Hero("Carlos", 0, i);
+}
